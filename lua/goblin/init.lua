eedf587b4cc77eda4b50f32e7cc20f47b52acc3c
Everton Jr <everton@grupoatt.com.br>
2024-01-29 15:34:19 -0300

    added more features, or not...

--- lua/goblin/init.lua
+++ lua/goblin/init.lua
@@ -1,7 +1,18 @@
 local M = {}
 
-M.setup = function()
+---@class Options
+---@field transparent boolean
+---@field term_colors boolean
+
+---Setup your beautiful colorscheme.
+---@param opts Options?
+M.setup = function(opts)
     print("Hi dear goblin")
+end
+
+---Load your beautiful colorscheme.
+M.load = function()
+    print("Loading...\nERROR! Gold missing in my pocket!")
 end
 
 return M
